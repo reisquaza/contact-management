f6e59fbf8eba76a749362133f6a34dc98076418c
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-02-05 00:38:32 -0300

    feat: routes-user

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,13 @@ import "reflect-metadata";
 import "express-async-errors";
 import * as express from "express";
 import handleAppErrorMiddleware from "./middlewares/handleAppError.middleware";
+import userRouter from "./routes/user.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/users", userRouter);
 
 app.use(handleAppErrorMiddleware);
 

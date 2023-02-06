ec589dc748f4307bafb0baa9431a2e57928ae54b
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-02-05 23:48:06 -0300

    feat: contacts-readAll

--- backend/src/routes/contacts.routes.ts
+++ backend/src/routes/contacts.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import createContactController from "../controllers/contacts/createContact.controller";
+import readAllContactsController from "../controllers/contacts/readAllContacts.controller";
 import tokenAuthMiddleware from "../middlewares/tokenAuth.middleware";
 
 const contactRouter = Router();
 
 contactRouter.post("/", tokenAuthMiddleware, createContactController);
+
+contactRouter.get("/users/:id", tokenAuthMiddleware, readAllContactsController);
 
 export default contactRouter;

1bc67f0f38d34798699b2fd275a6e6a592f20146
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-02-05 23:23:01 -0300

    feat: user-readById

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import createUserController from "../controllers/user/createUser.controller";
+import readUserByIdController from "../controllers/user/readUserById.controller";
 import readUsersController from "../controllers/user/readUsers.controller";
 
 const userRouter = Router();
@@ -7,5 +8,7 @@ const userRouter = Router();
 userRouter.post("/", createUserController);
 
 userRouter.get("/", readUsersController);
+
+userRouter.get("/:id", readUserByIdController);
 
 export default userRouter;
